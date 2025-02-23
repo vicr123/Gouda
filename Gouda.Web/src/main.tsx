8bfa83ea56ef0fadf95d31cd2a3b10527c14c662
Victor Tran <vicr12345@gmail.com>
2025-02-24 00:35:22 +1100

    Add i18n to frontend

--- Gouda.Web/src/main.tsx
+++ Gouda.Web/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import i18n from "i18next";
+import HttpApi, { HttpBackendOptions } from "i18next-http-backend";
 import "./index.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
+import { initReactI18next } from "react-i18next";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -15,6 +18,16 @@ declare module "@tanstack/react-router" {
         router: typeof router;
     }
 }
+
+i18n.use(initReactI18next)
+    .use(HttpApi)
+    .init<HttpBackendOptions>({
+        lng: "en",
+        fallbackLng: "en",
+        backend: {
+            loadPath: "/translations/{{lng}}/{{ns}}.json",
+        },
+    });
 
 // Render the app
 const rootElement = document.getElementById("root")!;
--- Gouda.Web/src/routes/about.tsx
+++ Gouda.Web/src/routes/about.tsx
@@ -1,9 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { useTranslation } from "react-i18next";
 
-export const Route = createFileRoute('/about')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/about")({
+    component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/about"!</div>
+    const { t } = useTranslation();
+    return <div>{t("TEST_TRANSLATION")}</div>;
 }
