c53dc461d0defaa13119cc0db2562fa7efb2d0a6
Victor Tran <vicr12345@gmail.com>
2025-02-27 23:23:36 +1100

    Implement member logs

---
+++ Gouda.Bot/Responders/MemberMovementResponder.cs
@@ -0,0 +1,48 @@
+using Gouda.Database;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.Extensions.Formatting;
+using Remora.Discord.Gateway.Responders;
+using Remora.Results;
+
+namespace Gouda.Bot.Responders;
+
+[UsedImplicitly]
+public class MemberMovementResponder(GoudaDbContext dbContext, IDiscordRestChannelAPI channelApi) : IResponder<IGuildMemberAdd>, IResponder<IGuildMemberRemove>
+{
+    public async Task<Result> RespondAsync(IGuildMemberAdd gatewayEvent, CancellationToken ct = default)
+    {
+        // Determine if we should be sending alert logs
+        var logsChannel = await dbContext.GuildChannels.FirstOrDefaultAsync(x => x.Id == gatewayEvent.GuildID.Value, cancellationToken: ct);
+        if (logsChannel?.AlertChannel is null)
+        {
+            return Result.Success;
+        }
+
+        await channelApi.CreateMessageAsync(
+            new(logsChannel.AlertChannel.Value),
+            $":arrow_right: {Mention.User(gatewayEvent.User.Value)}",
+            ct: ct);
+
+        return Result.Success;
+    }
+
+    public async Task<Result> RespondAsync(IGuildMemberRemove gatewayEvent, CancellationToken ct = default)
+    {
+        // Determine if we should be sending alert logs
+        var logsChannel = await dbContext.GuildChannels.FirstOrDefaultAsync(x => x.Id == gatewayEvent.GuildID.Value, cancellationToken: ct);
+        if (logsChannel?.AlertChannel is null)
+        {
+            return Result.Success;
+        }
+
+        await channelApi.CreateMessageAsync(
+            new(logsChannel.AlertChannel.Value),
+            $":arrow_left: {Mention.User(gatewayEvent.User)}",
+            ct: ct);
+
+        return Result.Success;
+    }
+}
